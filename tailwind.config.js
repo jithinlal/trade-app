1ae9583a7c6f694ad22a5b386509b173e5665fc7
jithinlal-gelato <jithinlal.pr@gelato.com>
2023-12-03 22:47:28 +0530

    chore: nextui custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,35 @@ module.exports = {
 		extend: {},
 	},
 	darkMode: 'class',
-	plugins: [nextui()],
+	plugins: [
+		nextui({
+			layout: {
+				spacingUnit: 4, // in px
+				disabledOpacity: 0.5, // this value is applied as opacity-[value] when the component is disabled
+				dividerWeight: '1px', // h-divider the default height applied to the divider component
+				fontSize: {
+					tiny: '0.75rem', // text-tiny
+					small: '0.875rem', // text-small
+					medium: '1rem', // text-medium
+					large: '1.125rem', // text-large
+				},
+				lineHeight: {
+					tiny: '1rem', // text-tiny
+					small: '1.25rem', // text-small
+					medium: '1.5rem', // text-medium
+					large: '1.75rem', // text-large
+				},
+				radius: {
+					small: '8px', // rounded-small
+					medium: '12px', // rounded-medium
+					large: '14px', // rounded-large
+				},
+				borderWidth: {
+					small: '1px', // border-small
+					medium: '2px', // border-medium (default)
+					large: '3px', // border-large
+				},
+			},
+		}),
+	],
 };
